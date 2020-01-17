f7aa4b97462d29f750787ab8c1d7266e43eac834
Borges <gustavoborges98@hotmail.com>
2020-01-17 02:10:12 -0300

    Backend e Frontend prontos e funcionando!
    Os perfils dos usuários cadastrados aparecem na tela instantaneamente após clicar em salvar;
    
    Estruturada no códico e criada pastas index.js e styles.css para melhor organização - em componentes -
    
    Código bem mais estruturado - componentes pais e filhos organizados;
    
    cadastro de usuários da aplicação web funcionando perfeitamente;

--- semanaoministack10/web/src/App.js
+++ semanaoministack10/web/src/App.js
@@ -6,157 +6,40 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 function App () {
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
 
+  useEffect(() =>{
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude} = position.coords;
+      setDevs(response.data);
+    }
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-
-
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout:30000,
-      }  
-    )
+   loadDevs(); 
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault()
-  
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude,
-    })
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
 
-    setGithubUsername('');
-    setTechs('');
-  
-    console.log(response.data);
+    setDevs([...devs, response.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input 
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-              />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input type = "number"
-                     name="latitude"
-                     id="latitude"
-                     required
-                     value={latitude} 
-                     onChange={e => setLatitude(e.target.value)}
-                     />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input type = "number"
-                     name="longitude"
-                     id="longitude"
-                     required
-                     value={longitude}
-                     onChange={e => setLongitude(e.target.value)}
-                     />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main> 
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src = "https://avatars2.githubusercontent.com/u/52687806?s=460&v=4" alt="Gustavo Borges"/>
-              <div className="user-info">
-                <strong>Gustavo Borges</strong>
-                <span>ReactJS, React Native, Node.js</span>
-                </div>
-            </header>
-            <p>Apaixonado por tecnologia!</p>
-            <a href="https://github.com/gustavobborges">Acessar perfil no Github</a>
-
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src = "https://avatars2.githubusercontent.com/u/52687806?s=460&v=4" alt="Gustavo Borges"/>
-              <div className="user-info">
-                <strong>Gustavo Borges</strong>
-                <span>ReactJS, React Native, Node.js</span>
-                </div>
-            </header>
-            <p>Apaixonado por tecnologia!</p>
-            <a href="https://github.com/gustavobborges">Acessar perfil no Github</a>
-
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src = "https://avatars2.githubusercontent.com/u/52687806?s=460&v=4" alt="Gustavo Borges"/>
-              <div className="user-info">
-                <strong>Gustavo Borges</strong>
-                <span>ReactJS, React Native, Node.js</span>
-                </div>
-            </header>
-            <p>Apaixonado por tecnologia!</p>
-            <a href="https://github.com/gustavobborges">Acessar perfil no Github</a>
-
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src = "https://avatars2.githubusercontent.com/u/52687806?s=460&v=4" alt="Gustavo Borges"/>
-              <div className="user-info">
-                <strong>Gustavo Borges</strong>
-                <span>ReactJS, React Native, Node.js</span>
-                </div>
-            </header>
-            <p>Apaixonado por tecnologia!</p>
-            <a href="https://github.com/gustavobborges">Acessar perfil no Github</a>
-
-          </li>
+          {devs.map(dev => (    
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
-
       </main>
     </div>
   );
