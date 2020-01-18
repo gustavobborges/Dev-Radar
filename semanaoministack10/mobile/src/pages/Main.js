3383a32e33f8276aea7be72a5f1701ab2150f76a
Borges <gustavoborges98@hotmail.com>
2020-01-18 00:21:03 -0300

    Criando Avatar no mapa
    Localização funcionando ao abrir o map;
    
    Enquanto a localização não for encontrada, o mapa não dá resposta;
    
    importado a imagem do meu usuário - enquadrado na tela e estilizado;
    
    importado o view e o text;
    
    informações Text de nome, techs e bio no avatar no mapa;

--- semanaoministack10/mobile/src/pages/Main.js
+++ semanaoministack10/mobile/src/pages/Main.js
@@ -1,38 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync}  from 'expo-location'
 
 
 function Main() {
     const [currentRegion, setCurrentRegion] = useState(null);
-    useEffect(() => {
-    async function loadInitialPosition() {
-        const {granted} = await requestPermissionsAsync();
-            
-            if (granted) {
-                const { coords } = await getCurrentPositionAsync({
-                    enableHighAccuracy: true,
-                });
 
-            const { latitude, longitude } = coords;
+        useEffect(() => {
+        async function loadInitialPosition() {
+            const {granted} = await requestPermissionsAsync();
+                
+                if (granted) {
+                    const { coords } = await getCurrentPositionAsync({
+                        enableHighAccuracy: true,
+                    });
 
-            
-        }
+                const { latitude, longitude } = coords;
+                
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                })     
+            }
+        } 
+
+        loadInitialPosition();
+    }, []);
+
+    if (!currentRegion) {
+        return null;
     }
 
 
-
-    
-    loadInitialPosition();
-    }, []);
-
-    return <MapView style={styles.map} />
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{ latitude: -27.6048586, longitude: -48.5170158 }}>
+                <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/52687806?s=460&v=4' }}></Image>
+                <Callout>
+                    <View style={styles.Callout}>
+                        <Text>Gustavo Borges</Text>
+                        <Text>Apaixonado por tecnologia!</Text>
+                        <Text>ReactJS, React Native, Node.js</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+    );
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1
+    },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
+
     },
 })
 
